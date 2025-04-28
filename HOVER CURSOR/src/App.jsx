04dedc23172a3fc3df278c4fb8077630100032cd
Himantha Thathsara <himanthathathsara2005@gmail.com>
2025-04-29 00:06:25 +0530

    App Files

---
+++ HOVER CURSOR/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div id="cursor"></div>
+      <button className="hover-link">
+        <span>
+          <span>
+            Hover<span>Hover</span>
+          </span>
+          <span>
+            Me<span>Me</span>
+          </span>
+        </span>
+      </button>
+    </>
+  );
+}
+
+export default App;
