0f2941881cc168f80f14df76ae1a96488f10914e
Himantha Thathsara <himanthathathsara2005@gmail.com>
2025-04-28 23:07:05 +0530

    App Files

---
+++ BUTTON HOVER EFFECT 2/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="button-container">
+          <span className="mask">HOVER</span>
+          <button type="button" name="Hover">
+            HOVER
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
