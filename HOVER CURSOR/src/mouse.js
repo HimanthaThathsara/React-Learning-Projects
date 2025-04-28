bad4f06762e09b3c3794b01c8a6eb767e5d04828
Himantha Thathsara <himanthathathsara2005@gmail.com>
2025-04-29 00:06:19 +0530

    Create mouse.js

---
+++ HOVER CURSOR/src/mouse.js
@@ -0,0 +1,13 @@
+document.addEventListener("mousemove", onMouseMove);
+let cursor = document.getElementById("cursor");
+const onMouseMove = (event) => {
+  cursor.style.left = event.pageX + "px";
+  cursor.style.top = event.pageY + "px";
+
+  if (event.target.hasAttribute("cursor-type")) {
+    let cursorSize = event.target.getAttribute("cursor-type");
+    cursor.classList.add(cursorSize);
+  } else {
+    cursor.classList = "";
+  }
+};
