8f0a0d1113a07897cbe3c10f0b828c97f31432ed
Himantha Thathsara <himanthathathsara2005@gmail.com>
2025-04-30 20:51:10 +0530

    Create main.jsx

---
+++ Animation Dashboard/src/main.jsx
@@ -0,0 +1,10 @@
+import './index.css'
+import Dashboard from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Dashboard />
+  </StrictMode>,
+)
