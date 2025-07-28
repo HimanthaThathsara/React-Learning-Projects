44ebf2f67f3841392effbb32273c3de01d38ab6e
Himantha Thathsara <himanthathathsara2005@gmail.com>
2025-07-28 18:13:35 +0530

    Create BgScene.jsx

---
+++ React Protfolio/src/pages/Home/Sections/Hero/comp/BgScene.jsx
@@ -0,0 +1,12 @@
+import Spline from "@splinetool/react-spline";
+
+export default function BgScene() {
+  return (
+    <Spline scene="https://prod.spline.design/1KRe4Z4U8LNbQeE9/scene.splinecode" />
+
+    // Uncomment one of the following lines to use a different Spline scene
+    // <Spline scene="https://my.spline.design/dunes-2ea1415c70da19c201f33da9268a0410/" />
+    // <Spline scene="https://my.spline.design/spaceparticlesanimation-c3acd83275e18c33c70e85d9d198be8e/" />
+    // <Spline scene="https://my.spline.design/glowingplanetparticles-a0c785ee0407074c17c66d95be23c018/" />
+  );
+}
