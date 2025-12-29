e58da5b664b62d149233d65441eb0fbd38e1eba4
Himantha Thathsara <himanthathathsara2005@gmail.com>
2025-12-29 13:16:04 +0530

    Add initial POS backend and frontend implementation
    Introduce a full-featured restaurant POS system with separate backend (Node.js/Express, MongoDB, Razorpay integration) and frontend (React, Redux, Tailwind). Includes user authentication, order, table, and payment management, as well as frontend UI components and assets.

---
+++ Restaurant_POS_System/pos-frontend/src/App.jsx
@@ -0,0 +1,50 @@
+import {BrowserRouter as Router, Routes, Route, useLocation, Navigate} from "react-router-dom";
+import {Home, Auth, Orders, Tables, Menu, Dashboard} from "./pages";
+import Header from "./components/shared/Header";
+import {useSelector} from "react-redux";
+import useLoadData from "./hooks/useLoadData";
+import FullScreenLoader from "./components/shared/FullScreenLoader";
+
+function Layout() {
+  const isLoading = useLoadData();
+  const location = useLocation();
+  const hideHeaderRoutes = ["/auth"];
+  const {isAuth} = useSelector((state) => state.user);
+
+  if (isLoading) return <FullScreenLoader />;
+
+  return (
+    <>
+      {!hideHeaderRoutes.includes(location.pathname) && <Header />}
+      <Routes>
+        {/* use ProtectedRoutes if you want */}
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={isAuth ? <Navigate to="/" /> : <Auth />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/tables" element={<Tables />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+    </>
+  );
+}
+
+function ProtectedRoutes({children}) {
+  const {isAuth} = useSelector((state) => state.user);
+  if (!isAuth) {
+    return <Navigate to="/auth" />;
+  }
+
+  return children;
+}
+
+function App() {
+  return (
+    <Router>
+      <Layout />
+    </Router>
+  );
+}
+
+export default App;
